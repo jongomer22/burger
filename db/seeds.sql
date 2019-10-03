9660a8d3c531bad8a88240cdb6256280fe4811b2
JonGomer <jongomer22@gmail.com>
2019-10-02 23:21:43 -0700

    Schema Seeds and DB Setup

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Double Double", TRUE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Animal Style", TRUE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Protein Style", FALSE);
